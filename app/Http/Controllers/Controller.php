0655cc5239d3b14f064a85f74adb0c58ac003397
Pau Muñoz Serra <147044090+MunozSerraPau@users.noreply.github.com>
2025-04-25 20:56:08 +0200

    problema arreglar de rutes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,13 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 
 abstract class Controller extends \Illuminate\Routing\Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 }
